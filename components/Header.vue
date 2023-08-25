20d48925383ed268e723416ae3cd9d3f4f4c17c5
triistam <isakulovdev@gmail.com>
2023-08-25 23:49:56 +0500

    nigger Header

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-    <div class="flex">
-        <h1>Genemator's</h1>
+    <div class="bg-gray-900 text-white  w-100  p-10">
+        <div class="container flex justify-between items-center">
+        <nuxt-link to="/">
+            <h1 class="text-4xl mx-5">Genemator's</h1>
+        </nuxt-link>
+            <ul class="mx-5">
+            <li>
+                <nuxt-link to="/" class="link">Home</nuxt-link>
+                <nuxt-link to="/projects" class="link">About</nuxt-link>
+                <nuxt-link to="/blog" class="link">Blog</nuxt-link>
+            </li>
+        </ul>
+    </div>
     </div>
 </template>
 
-<style></style>
+<style scoped>
+.link {
+   margin: 20px;
+   font-size: 20px;
+}
+
+* {
+    color: white;
+}
+
+.bg-dark {
+    background-color: black;
+}
+</style>
