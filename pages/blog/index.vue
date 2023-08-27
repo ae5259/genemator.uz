12324e14f93225df44ef0da6a121a77993439793
triistam <isakulovdev@gmail.com>
2023-08-27 21:34:24 +0500

    fixed syntax error and replaced nuxt-link with NuxtLink

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,11 +10,11 @@
     <div class=" w-10/12 mx-auto  p-5 text-white">
         <ContentList path="/blog" v-slot="{ list }">
             <div v-for="blog in list" :key="blog._path" class="my-5 p-5 border bg-gray-900 rounded-2xl">
-            <nuxt-link to={{ blog.slug }}>
+            <NuxtLink :to="blog.slug">
                 <h1 class="text-3xl">{{ blog.title }}</h1>
                 <br>
                 <p class="text-xl">{{ blog.description }}</p>
-            </nuxt-link>
+            </NuxtLink>
             </div>
         
         </ContentList>
