1935e2dbdace75bb99db6c5bdb59ba3d8a88dcaa
triistam <isakulovdev@gmail.com>
2023-08-27 21:34:01 +0500

    replace nuxt-link with NuxtLink to newer

--- components/Header.vue
+++ components/Header.vue
@@ -4,14 +4,14 @@
 <template>
     <div class="bg-gray-900 text-white  w-100  p-10">
         <div class="container flex justify-between items-center">
-        <nuxt-link to="/">
+        <NuxtLink to="/">
             <h1 class="text-4xl mx-5">Genemator's</h1>
-        </nuxt-link>
+        </NuxtLink>
             <ul class="mx-5">
             <li>
-                <nuxt-link to="/" class="link">Home</nuxt-link>
-                <nuxt-link to="/projects" class="link">About</nuxt-link>
-                <nuxt-link to="/blog" class="link">Blog</nuxt-link>
+                <NuxtLink to="/" class="link">Home</NuxtLink>
+                <NuxtLink to="/projects" class="link">About</NuxtLink>
+                <NuxtLink to="/blog" class="link">Blog</NuxtLink>
             </li>
         </ul>
     </div>
