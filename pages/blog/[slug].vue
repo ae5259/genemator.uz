35a0bf68cbfddfd544cbaf83cf94084ba38ea51e
triistam <isakulovdev@gmail.com>
2023-08-28 10:04:20 +0500

    posts are optimized for dark mode

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,15 +1,24 @@
 <template>
   <main>
     <Header></Header>
-    <div class="w-10/12 mx-auto  p-5 ">
-      <article class="prose mx-auto">
+    <div class="w-10/12 mx-auto p-5 text-white">
+      <article class="prose mx-auto text-white">
         <ContentDoc v-slot="{ doc }">
-          <h1 class="text-4xl text-center p-3">{{ doc.title }}</h1>
-          <div class="text-xl">
-            <ContentRenderer :value="doc" />
+          <h1 class="text-4xl text-center p-3 text-white">{{ doc.title }}</h1>
+          <div class="text-xl text-white">
+            <ContentRenderer :value="doc" class="text-white"/>
           </div>
         </ContentDoc>
       </article>
     </div>
   </main>
 </template>
+
+<style>
+
+.prose :where(code):not(:where([class~="not-prose"] *)),
+.prose :where(a):not(:where([class~="not-prose"] *))
+{
+  color: #e5e5e5 !important;
+}
+</style>
